2c5a24f09cbb1363a1e09d2ace3d6a55209bcfe5
mrduduka918-dot <mrduduka918@gmail.com>
2026-07-15 18:57:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY 
 );
 
 app.get("/", async (req, res) => {
