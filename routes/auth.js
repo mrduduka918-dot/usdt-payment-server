b9a956b95bf4ee5e9161df0bfdd8a12ec60ad3ef
mrduduka918-dot <mrduduka918@gmail.com>
2026-07-21 17:36:07 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,17 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+
+    res.sendFile(require("path").join(__dirname, "../public/signup.html"));
+
+});
+
+router.get("/login", (req, res) => {
+
+    res.sendFile(require("path").join(__dirname, "../public/login.html"));
+
+});
+
+module.exports = router;
